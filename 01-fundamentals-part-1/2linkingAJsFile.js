e48d117dfc2f56cbafad1cd59e925da65fa440ae
vinod_0808 <vinodpatidar0808@gmail.com>
2022-05-13 19:37:17 +0530

    02 link a js file with html file

---
+++ 01-fundamentals-part-1/2linkingAJsFile.js
@@ -0,0 +1,8 @@
+// this file will be linked in the html page where we want to use it, check index.html for linking
+
+let js = 'amazing';
+if (js === 'amazing') {
+    alert('js is fun');
+}
+// console.log(); prints contents inside parens to browser console
+console.log(50 + 20 + 10);
