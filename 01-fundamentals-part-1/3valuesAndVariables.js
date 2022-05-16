1a2e5f05293feeb77446ef905e54c17269c5a683
vinod_0808 <vinodpatidar0808@gmail.com>
2022-05-16 23:09:45 +0530

    03 - values and variables

---
+++ 01-fundamentals-part-1/3valuesAndVariables.js
@@ -0,0 +1,29 @@
+//value is a piece of information
+// 'vinod' -->this is a value
+//  you can have different values
+// this 4 numbers are also different values
+console.log(40 - 23 + 10 + 1);
+
+//variable
+let variableName = 'Vinod';
+//above line creates a variable in your computer's memory with value = vinod
+
+let lastName = 'Patidar';
+// you can use varible as many times as you like in your program
+// this will print value of variable lastName
+console.log(lastName);
+console.log(lastName);
+console.log(lastName);
+
+//with variables you whenever you need to change value of lastName you have to change it only once
+
+//conventions and rules of variable naming
+//  1. camel casing: first letter small and rest words have first letter in caps like firstName , if only one word in variable-> first/name etc
+//  2. you can use underScores also --> but camelCasing is standard in js ecosystem
+//  3. you can not have first letter as a number like -> 3bowls-> invalid, syntax error
+//  4. varibales can only contain numbers, letters (caps and small ) , underscores. and dollar ($) sign only. something like this fist&name will be invalid
+//  5.  variable can not be named as reserved keywords like new , class etc
+//  6. name is also reserved keyword
+//  7. never start variable name with first letter in caps
+//  8. constants like PI should be named  in all caps
+//  9. make your variable names descriptive -> not required by js 😊, but it's a good practice to write readable and clean code
