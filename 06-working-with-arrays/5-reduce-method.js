f47affc5e698bed20375bcd34cce3c6ade1cf004
vinod_0808 <vinodpatidar0808@gmail.com>
2022-06-15 16:10:42 +0530

    reduce method

---
+++ 06-working-with-arrays/5-reduce-method.js
@@ -0,0 +1,33 @@
+'use strict';
+
+// reduce method: reduces the entire array to one element, like adding all elements or any other operation
+// reduce also gets the callback function but here in callback function we have first parameter as accumulator and 2nd is element, 3rd element index and 4th entire array
+// reduce gets 2 parameters first is callback function and 2nd is initial value of accumulator
+// you can have both regular and arrow function for callback
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// accumulator is like a snowball
+const balance = movements.reduce((acc, curr, i, arr) => {
+    console.log(`iteration ${i + 1}: acc = ${acc}`);
+    return acc + curr;
+}, 100);
+
+console.log(movements);
+console.log(balance);
+
+// same thing using for loop
+/* 
+let totalBalance = 0;
+for (const mov of movements) {
+    totalBalance += mov;
+}
+console.log(totalBalance);
+ */
+
+// we can do other things also with reduce
+
+const maxValue = movements.reduce((maxVal, curr) => {
+    if (maxVal < curr) maxVal = curr;
+    console.log(maxVal);
+    return maxVal;
+}, movements[0]);
+console.log(maxValue);
