fa707d313d25c0ee6d793aa77e97e6fcaf8bdf8d
vinod_0808 <vinodpatidar0808@gmail.com>
2022-06-23 18:59:56 +0530

    commonJs module

--- 10-modernJS-development-tooling-modules-functional/modulePattern.js
+++ 10-modernJS-development-tooling-modules-functional/modulePattern.js
@@ -31,3 +31,23 @@ console.log(shoppingCart2);
 console.log(shoppingCart2.shippingCost);
 
 //for each different type of module you had to create different script for each type of module and also had to include the scripts inside your html, they had problem with bundling also
+
+//-----------------------COMMMONJS MODULE------------------------------
+/*
+
+// NOTE: commonJS module system --> used widely before ES6 and nodejs also used to have modules based on commonJS, most of npm packages still use commonJS module as npm  initially was intended for node only. but now it is used for entire js ecosystem
+// in commonJS one file is one module
+// this won't work in our code and browser, but it will work with nodeJs
+// export
+const cart2 = [];
+const shippingCost2  =60;
+export.addToCart2 =function (product, quantity) {
+    cart2.push({product, quantity});
+    console.log(`${quantity} ${product} added to cart (shipping cost is ${shippingCost2})`)
+}
+
+// import : require is not defined in browser environment but it works well with NodeJS
+const {addToCart } = require('./shoppingCart.js')
+
+
+*/
